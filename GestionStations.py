3caee01162c8e92cad1368c6df4185cfde05fe80
Arthur Royer <arthur.royer92@orange.fr>
2024-10-15 14:40:48 +0200

    Maj

--- GestionStations.py
+++ GestionStations.py
@@ -1,6 +1,8 @@
+from geopy.distance import geodesic
+
+
 def gestion_stations(stations_data):
-    start_station = start(stations_data)
-    end_station = end(stations_data)
+    start_station, end_station = end(stations_data)
 
     return start_station, end_station
 
@@ -17,12 +19,36 @@ def start(stations_data):
 
 def end(stations_data):
     list_percentage = get_list_percentage(stations_data)
+    list_index_min = []
+    list_index_zero = []
+    last_distance = 1000000.0
+    end_station = stations_data[2]
 
-    min_percentage = min(list_percentage)
+    for i in range(len(list_percentage)):
+        percentage = list_percentage[i]
+        if percentage == 0:
+            list_index_zero.append(i)
+        elif 1 <= percentage < 25:
+            list_index_min.append(i)
 
-    end_station = stations_data[list_percentage.index(min_percentage) + 2]
+    start_station = start(stations_data)
 
-    return end_station
+    if list_index_zero:
+        list_index = list_index_zero
+    else:
+        list_index = list_index_min
+
+    for index in list_index:
+        station = stations_data[index + 2]
+        start_coord = [float(start_station['position']['lat']), float(start_station['position']['lng'])]
+        end_coord = [float(station['position']['lat']), float(station['position']['lng'])]
+        distance = geodesic(start_coord, end_coord).kilometers
+
+        if distance < last_distance:
+            last_distance = distance
+            end_station = station
+
+    return start_station, end_station
 
 
 def get_list_percentage(stations_data):
